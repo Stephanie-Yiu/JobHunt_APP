974423e9d97ac280c283c85352766f921d686143
Stephanie-Yiu <116084131+Stephanie-Yiu@users.noreply.github.com>
2024-01-04 00:06:00 +0800

    hash password with error handle

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+///naming the rouete from  route, it was "export default router"
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 mongoose
  .connect(process.env.MANGODB)
  .then(() => {
@@ -13,8 +17,21 @@ mongoose
  });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
  console.log("you're at port 3000");
 });
-//see me!!
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+ const statusCode = err.statusCode || 500;
+ const message = err.message || 'Internal Server Error';
+ return res.status(statusCode).json({
+  success: false,
+  statusCode: statusCode,
+  message,
+ });
+}); // Error handling function
